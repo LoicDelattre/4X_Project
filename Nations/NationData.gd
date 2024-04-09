16ce38ca329d54c5e4deedfe46a0bef7fe854b91
lolodel <loicdelattre38@gmail.com>
2024-04-09 16:17:25 +0200

    commit prebranch out

--- Nations/NationData.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name NationData
-
-@export var nationID : int = 0
-@export var baseMoney : int = 0
- 
--- global.gd
+++ global.gd
@@ -2,12 +2,8 @@ extends Node
 
 @export_range(1,5) var gameSpeed : int = 3
 var gameIteration : int = 1
-var gameSpeedChange : bool = false
 
-var playerNationID : int = 0
-var nationsID : Array = ["Spectator", "Iceland", "Norway", "Sweden", "Finland", "Russia", "Ireland", "United Kingdom",
-						"Danemark", "Lithuania", "Latvia", "Estonia", "France", "Belgium", "Netherlands", "Germany", 
-						"Poland"]
+var gameSpeedChange : bool = false
 
 var overlayMapMode : int = 0
 var mapModes : Array = ["Countries", "Military"]
